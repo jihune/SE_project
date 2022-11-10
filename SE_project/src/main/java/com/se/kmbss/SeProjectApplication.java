daf0bacd87561d96ffcd6c1b01aec481d53ae7bc
jihune <jihun7891@naver.com>
2022-11-10 22:23:40 +0900

    주석 추가 2

--- SE_project/src/main/java/com/se/kmbss/SeProjectApplication.java
+++ SE_project/src/main/java/com/se/kmbss/SeProjectApplication.java
@@ -3,6 +3,14 @@ package com.se.kmbss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 프로그램을 시작하면 가장먼저 시작하게 된다.
+// 건드릴 일은 거의 없다.
+
+// src/main/resources/ 의 파일구조 설명
+// mapper: 각종 쿼리문을 모으는 폴더
+// static js, css파일을 모으는 폴더
+// template: html파일을 모으는 폴더
+
 @SpringBootApplication
 public class SeProjectApplication {
 
--- SE_project/src/main/java/com/se/kmbss/controller/MainController.java
+++ SE_project/src/main/java/com/se/kmbss/controller/MainController.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 각 html 페이지를 연결해 주는 곳이다.
-// 추후 사용자가 사용하는 페이지를 다루는 UserController 추가예정
-// 정말 간단한 것만 구현되어 있고 추후 Mapper와 연결해야 함
+// 사용자가 사용하는 페이지를 다루는 UserController 추가예정
+// 추후 Mapper와 작성해서 DB와 연결
+
+// 현재는 간단한 링크 이동만 가능, 뒤로가기나 여러 페이지로 연결 등 html 완성되면 매끄럽게 다듬어야 함
+// 재성이 필요할 경우, 아래 home 함수 복사해서 이름 3개만 바꿔서 html 연결하셈
 
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "/home")
-    public String home(){
+    
+    @RequestMapping(value = "/home")    // localhost 링크
+    public String home(){               // 함수이름
 
-        return "index.html";
+        return "index.html";            // html 소스
     }
 
 	@RequestMapping(value = "/sign_up")
@@ -22,10 +26,10 @@ public class MainController {
         return "sign_up.html";
     }
 
-	@RequestMapping(value = "/login")
-    public String login(){
+	@RequestMapping(value = "/sign_in")
+    public String sign_in(){
 
-        return "login.html";
+        return "sign_in.html";
     }
 
 }
