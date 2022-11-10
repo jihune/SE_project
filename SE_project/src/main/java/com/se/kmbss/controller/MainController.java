81917feeb80919981c89c7fc2a9d356ed8920b4a
jihune <jihun7891@naver.com>
2022-11-10 21:22:49 +0900

    인덱스 페이지 컨트롤 구현 완료

--- SE_project/src/main/java/com/se/kmbss/controller/MainController.java
+++ SE_project/src/main/java/com/se/kmbss/controller/MainController.java
@@ -1,50 +1,27 @@
 package com.se.kmbss.controller;
 
-// import java.io.IOException;
-// import java.util.List;
-
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.ResponseBody;
-// import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/")
-
 public class MainController {
-    
-    /*
-    @Autowired
-	MainMapper mapper;
-    */
 
-    @GetMapping
-	public String index(Model model) {
-		return "index";
-	}
+    @RequestMapping(value = "/home")
+    public String home(){
 
-	/**
-	 * @return 회원가입 페이지
-	 */
-	@GetMapping("sign_up")
-	public String sign_up() {
-		return "sign_up";
-	}
+        return "index.html";
+    }
 
-    /*
-	@PostMapping("sign_up")
-	public String sign_up(CustomerDTO customer) {
-		System.out.println(customer);
-		if(customer.getGeneralCall() == null){
-			customer.setGeneralCall("053...01");
-		}
-		customerService.join(customer);
-		return "redirect:/";
-	}
-    */
+	@RequestMapping(value = "/sign_up")
+    public String sign_up(){
+
+        return "sign_up.html";
+    }
+
+	@RequestMapping(value = "/login")
+    public String login(){
+
+        return "login.html";
+    }
+
 }
