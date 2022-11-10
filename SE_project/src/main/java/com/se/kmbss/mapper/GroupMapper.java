f3212da6f8a0017c40a07e6b51fdd49679583c66
jihune <jihun7891@naver.com>
2022-11-10 22:23:24 +0900

    주석 추가

--- SE_project/src/main/java/com/se/kmbss/mapper/GroupMapper.java
+++ SE_project/src/main/java/com/se/kmbss/mapper/GroupMapper.java
@@ -1,5 +1,6 @@
 package com.se.kmbss.mapper;
 
+// 커뮤니티 기능 구현 후 작성, 그룹 관리
 public class GroupMapper {
     
 }
--- SE_project/src/main/java/com/se/kmbss/mapper/UserMapper.java
+++ SE_project/src/main/java/com/se/kmbss/mapper/UserMapper.java
@@ -4,18 +4,19 @@ package com.se.kmbss.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-// import com.shoping.dbckd.model.CustomerDTO; // UserDTO 만들면 수정 
+// import com.shoping.dbckd.model.CustomerDTO;
 
-/**
- * 회원에 관한 동작을 정의하는 인터페이스입니다.
- */
 
+// 회원에 관한 동작을 정의하는 인터페이스입니다.
 @Mapper
 public interface UserMapper {
 
-    /* 아래는 UserMapper의 Example로 가져온 쇼핑몰의 CustomerMapper
-    UserDTO 만들면 아래처럼 구현해두기
+    /*
+        아래는 UserMapper의 Example로 가져온 쇼핑몰의 CustomerMapper
+        UserDTO 만들면 Example처럼 구현해두기
+    */
 
+    /* 
     public Integer join(CustomerDTO customer);
     public List<CustomerDTO> checkID(CustomerDTO customer);
     public List<CustomerDTO> checkNick(CustomerDTO customer);
