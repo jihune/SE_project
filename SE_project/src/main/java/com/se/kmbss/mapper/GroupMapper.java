f65a7e35d9e8b74c9f986e022462e0c19f3a3948
jihune <jihun7891@naver.com>
2022-11-15 19:13:51 +0900

    model 폴더 속 UserDTO

--- SE_project/src/main/java/com/se/kmbss/mapper/GroupMapper.java
+++
@@ -1,8 +0,0 @@
-package com.se.kmbss.mapper;
-
-// 커뮤니티 기능 구현 후 작성
-
-// 그룹에 관한 동작을 정의하는 클래스입니다.
-public class GroupMapper {
-
-}
--- SE_project/src/main/java/com/se/kmbss/serviceImplements/UserServiceImpl.java
+++ SE_project/src/main/java/com/se/kmbss/serviceImplements/UserServiceImpl.java
@@ -1,66 +1,64 @@
-// 손지훈 작업중
+package com.se.kmbss.serviceImplements;
 
-// package com.se.kmbss.serviceImplements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import com.se.kmbss.mapper.UserMapper;
+import com.se.kmbss.model.UserDTO;
+import com.se.kmbss.service.UserService;
 
-// import com.se.kmbss.mapper.UserMapper;
-// import com.se.kmbss.model.UserDTO;
-// import com.se.kmbss.service.UserService;
+@Service
+public class UserServiceImpl implements UserService {
 
-// @Service
-// public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper user_mapper;
 
-//     @Autowired
-//     UserMapper user_mapper;
+    @Override
+    public Integer join(UserDTO user){
+        return user_mapper.join(user);
+    }
 
-//     @Override
-//     public Integer join(UserDTO user){
-//         return user_mapper.join(user);
-//     }
+    @Override
+    public boolean isUniqueId(UserDTO user) {
+        if(user_mapper.checkId(user).size() == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-//     @Override
-//     public boolean isUniqueID(UserDTO user) {
-//         if(user_mapper.checkID(user).size() == 0){
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
+    @Override
+    public boolean isOverlapNick(UserDTO user) {
+        if(user_mapper.checkNick(user).size() == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-//     @Override
-//     public boolean is_overlap_nick(UserDTO user) {
-//         if(user_mapper.is_overlap_nick(user).size() == 0){
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
+    @Override
+    public UserDTO login(UserDTO user){
+        return user_mapper.login(user);
+    }
 
-//     @Override
-//     public UserDTO login(UserDTO user){
-//         return user_mapper.login(user);
-//     }
+    @Override
+    public boolean isOverlapEmail(UserDTO user) {
+        if(user_mapper.checkEmail(user).size() == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-//     @Override
-//     public boolean is_overlap_email(UserDTO user) {
-//         if(user_mapper.check_email(user).size() == 0){
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
+    @Override
+    public String findMyId(UserDTO user) {
+        UserDTO result = user_mapper.findMyId(user);
+        System.out.println(result);
+        return result.getId();
+    }
 
-//     @Override
-//     public String find_my_id(UserDTO user) {
-//         UserDTO result = user_mapper.find_my_id(user);
-//         System.out.println(result);
-//         return result.getId();
-//     }
-
-//     @Override
-//     public Integer change_my_info(UserDTO user) {
-//         return user_mapper.change_my_info(user);
-//     }
-// }
+    @Override
+    public Integer changeMyInfo(UserDTO user) {
+        return user_mapper.changeMyInfo(user);
+    }
+}
