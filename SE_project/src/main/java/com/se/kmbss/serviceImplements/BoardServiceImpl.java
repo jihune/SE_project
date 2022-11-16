7d32bf475a8040827a3b46c2fdab6d52299a2e5e
MKMin <headwig9898@naver.com>
2022-11-17 03:32:07 +0900

    게시물 수정이랑 삭제랑 했어 근데 앞에도 적었다시피 return 값은 다른데 이거는 재성이가 만든 html파일 그거랑 연결해야될듯 근데 내가 아직 확실하게는 몰라서 그부분은 그냥 그대로 놔둘게

--- SE_project/src/main/java/com/se/kmbss/serviceImplements/BoardServiceImpl.java
+++ SE_project/src/main/java/com/se/kmbss/serviceImplements/BoardServiceImpl.java
@@ -26,4 +26,24 @@ public class BoardServiceImpl implements BoardService{
         return dao.list();
     }
 
+    //게시물 조회
+    @Override
+    public BoardVO read(int bno) throws Exception {
+
+        return dao.read(bno);
+    }
+
+    //게시물 수정
+	@Override
+	public void update(BoardVO boardVO) throws Exception {
+
+		dao.update(boardVO);
+	}
+
+    //게시물 삭제
+	@Override
+	public void delete(int bno) throws Exception {
+		
+		dao.delete(bno);
+	}
 }
