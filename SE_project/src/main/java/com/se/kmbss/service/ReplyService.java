bcc7b8689af330edc89149d1c96838d4117e6db0
jihune <jihun7891@naver.com>
2022-11-17 16:21:49 +0900

    Revert "일단...게시판 글쓰는거랑 조회 해보긴했는데 BoardController에서 return값에 지금return "board/list"; 이런식으로 적었는데 이거는 손좀 봐야될거같아"
    This reverts commit e7feb5e3b99c5c833f84e9637061fdd3edb89961.

--- SE_project/src/main/java/com/se/kmbss/service/ReplyService.java
+++
@@ -1,7 +0,0 @@
-package com.se.kmbss.service;
-import java.util.List;
-import com.se.kmbss.model.ReplyVO;
-
-public interface ReplyService {
-    public List<ReplyVO> readReply(int bno) throws Exception;
-}
