f90813ac4226a8001094785bf2e3831b63486069
Cury <okn369258@naver.com>
2022-11-23 02:33:21 +0900

    board controller 파일 생성및 작성 오류나면 삭제 요망

--- SE_project/src/main/java/com/se/kmbss/controller/BoardController.java
+++ SE_project/src/main/java/com/se/kmbss/controller/BoardController.java
@@ -1,20 +1,31 @@
 package com.se.kmbss.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.se.kmbss.service.BoardService;
+import com.se.kmbss.mapper.BoardResponse;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
-    
     private final BoardService BoardService;
+    
+    // @GetMapping(".do")
+    // public String openBoardWrite(Model model){
+    //     return "notice_board";
+    // }
 
-    @GetMapping
-    public String openBoardWrite(Model model){
-        return "Board/Write";
+    @GetMapping("notice_board.do")
+    public String openBoardList(Model model){
+        List<BoardResponse> posts=BoardService.findAll();
+        model.addAttribute("Board", posts);
+        return "notice_board";
     }
-
+    
 }
+
