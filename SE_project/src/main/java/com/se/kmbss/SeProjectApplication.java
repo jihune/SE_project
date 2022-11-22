9e730c0e38cb80242ca956be4db3621a5c0c9641
jihune <jihun7891@naver.com>
2022-11-23 08:22:22 +0900

    !! 중요 !! application.properties 속 주석 참고하여 DB 접속 경로 수정

--- SE_project/src/main/java/com/se/kmbss/SeProjectApplication.java
+++ SE_project/src/main/java/com/se/kmbss/SeProjectApplication.java
@@ -11,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // static js, css파일을 모으는 폴더
 // template: html파일을 모으는 폴더
 
+// !! 중요 !!
+// 작업 환경이 달라졌을 경우 (처음 프로젝트 다운로드 받은 경우)
+// DB접속을 위해  SE_project/src/main/resources/application.properties 파일 반드시 수정 후 실행
+// 수정 방법은 해당 파일의 주석 참고
+
 @SpringBootApplication
 public class SeProjectApplication {
 
