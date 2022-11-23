15d3ba5d20f01ad4b1b329c960b54668d8f54f51
Cury <okn369258@naver.com>
2022-11-24 00:56:19 +0900

    board controller오류 주석 지우면 실행 안됨

--- SE_project/src/main/java/com/se/kmbss/controller/BoardController.java
+++ SE_project/src/main/java/com/se/kmbss/controller/BoardController.java
@@ -19,27 +19,27 @@ import com.se.kmbss.mapper.BoardResponse;
 public class BoardController {
     private final BoardService BoardService;
 
-    @PostMapping("notice_detailpage")
-    public String uploadBoard(final BoardRequest params){
-        BoardService.uploadBoard(params);
-        return "redirect:/notice_board";
-    }
+    // @PostMapping("notice_detailpage")
+    // public String uploadBoard(final BoardRequest params){
+    //     BoardService.uploadBoard(params);
+    //     return "redirect:/notice_board";
+    // }
 
-    @GetMapping("notice_detailpage")
-    public String openBoardWrite(@RequestParam(value = "board_idn", required = false) final int board_idn,Model model){
-        if(board_idn != 0){
-            BoardResponse post = BoardService.find_by_id(board_idn);
-            model.addAttribute("post", post);
-        }
-        return "notice_detailpage";
-    }
+    // @GetMapping("notice_detailpage")
+    // public String openBoardWrite(@RequestParam(value = "board_idn", required = false) final int board_idn,Model model){
+    //     if(board_idn != 0){
+    //         BoardResponse post = BoardService.find_by_id(board_idn);
+    //         model.addAttribute("post", post);
+    //     }
+    //     return "notice_detailpage";
+    // }
 
-    @GetMapping("notice_board.do")
-    public String openBoardList(Model model){
-        List<BoardResponse> posts=BoardService.findAll();
-        model.addAttribute("posts", posts);
-        return "notice_board";
-    }
+    // @GetMapping("notice_board.do")
+    // public String openBoardList(Model model){
+    //     List<BoardResponse> posts=BoardService.findAll();
+    //     model.addAttribute("posts", posts);
+    //     return "notice_board";
+    // }
     
 }
 
