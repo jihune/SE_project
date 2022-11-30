bbfbf4ec7d699998246f1d4e66f1380a2b31c807
jihune <jihun7891@naver.com>
2022-12-01 01:10:06 +0900

    포크 이후 첫 정상 동작 확인 커밋

--- src/main/java/com/se/kmbss/SeProjectApplication.java
+++ src/main/java/com/se/kmbss/SeProjectApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 // 프로그램을 시작하면 가장먼저 시작하게 된다.
 // 건드릴 일은 거의 없다.
 
-// src/main/resources/ 의 파일구조 설명
+// src/main/resources/ 의 파일구조
 // mapper: 각종 쿼리문을 모으는 폴더
-// static js, css파일을 모으는 폴더
+// static: js, css파일을 모으는 폴더
 // template: html파일을 모으는 폴더
 
 // DB접속 IO오류 발생 시
